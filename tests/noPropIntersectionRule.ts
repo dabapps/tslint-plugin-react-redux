7266d3cc614376fa3791544a040b628b443e7314
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-23 21:11:05 +0100

    Begin testing rules

---
+++ tests/noPropIntersectionRule.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+import * as Lint from 'tslint';
+import * as ts from 'typescript';
+import { Rule as NoPropIntersection } from '../src/noPropIntersectionRule';
+
+describe('no-prop-intersection', () => {
+  jest.spyOn(Lint.RuleWalker.prototype, 'addFailure');
+
+  beforeEach(() => {
+    (Lint.RuleWalker.prototype.addFailure as jest.Mock).mockClear();
+  });
+
+  it('should do some things', () => {
+    expect(Lint.RuleWalker.prototype.addFailure).not.toHaveBeenCalled();
+
+    const filePath = path.resolve(__dirname, '../test-files/failure.tsx');
+    const sourceText = ts.sys.readFile(filePath, 'utf8');
+
+    if (typeof sourceText === 'undefined') {
+      throw new Error('Could not get test file contents');
+    }
+
+    const sourceFile = ts.createSourceFile(
+      path.basename(filePath),
+      sourceText,
+      ts.ScriptTarget.Latest,
+      true,
+      ts.ScriptKind.Unknown
+    );
+
+    const ruleOptions: Lint.IOptions = {
+      ruleArguments: [],
+      ruleSeverity: 'error',
+      ruleName: 'no-prop-intersection',
+      disabledIntervals: []
+    };
+
+    const instance = new NoPropIntersection(ruleOptions);
+
+    instance.apply(sourceFile);
+
+    expect(Lint.RuleWalker.prototype.addFailure).toHaveBeenCalledTimes(1);
+  });
+});
